9abce593b18c6a259627e05518ab7d189cda798f
ShitalKarad <shitalkarad24@gmail.com>
2022-12-30 21:58:35 +0530

    Added: last name valid

--- com/brigelabz/Regex.java
+++ com/brigelabz/Regex.java
@@ -4,8 +4,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex {
-    static Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+
     public  void validFirstName() {
+         Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
         Matcher matcher = pattern.matcher("Shital");
         if (matcher.matches()){
             System.out.println("valid");
@@ -13,5 +14,13 @@ public class Regex {
             System.out.println("invalid");
         }
     }
-
+    public  void validLastName() {
+        Pattern pattern = Pattern.compile("^[A-Z]{1}[a-z]{3,15}$");
+        Matcher matcher = pattern.matcher("Shital");
+        if (matcher.matches()){
+            System.out.println("valid");
+        }else {
+            System.out.println("invalid");
+        }
+    }
 }
--- com/brigelabz/RegexMain.java
+++ com/brigelabz/RegexMain.java
@@ -4,5 +4,6 @@ public class RegexMain {
     public static void main(String args[]){
         Regex regex = new Regex();
         regex.validFirstName();
+        regex.validLastName();
     }
 }
